3d0decd57faffd0529b3c7be447229c2d53d97e8
VladELM <vladislav.elmanov@outlook.com>
2025-08-15 20:06:12 +0300

    change3

--- Assets/Scripts/Character/Health.cs
+++ Assets/Scripts/Character/Health.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] protected int _healthValue;
+    [SerializeField] protected int _health;
+    [SerializeField] private int _healingValue;
+    [SerializeField] private int _damagingValue;
+    [SerializeField] private float _healingTime;
+    [SerializeField] private float _damagingTime;
+    
+    private int _targetHealth;
+    private Coroutine _healCoroutine;
+    private Coroutine _damageCoroutine;
+    private WaitForSeconds _healingDelay;
+    private WaitForSeconds _damagingDelay;
+    private bool _isHealing;
+    private bool _isDamaging;
 
     public event Action<int> HealthChanged;
     public event Action<int> MaxHealthAssigned;
@@ -15,7 +28,11 @@ public class Health : MonoBehaviour
 
     private void Awake()
     {
-        MaxHealthValue = _healthValue;
+        MaxHealthValue = _health;
+        _healingDelay = new WaitForSeconds(_healingTime);
+        _damagingDelay = new WaitForSeconds(_damagingTime);
+        _isHealing = false;
+        _isDamaging = false;
     }
 
     private void Start()
@@ -26,44 +43,97 @@ public class Health : MonoBehaviour
 
     public void Heal(int healPoints)
     {
-        if (_healthValue < MaxHealthValue)
+
+        if (_isHealing)
+            StopCoroutine(_healCoroutine);
+
+        if (_isDamaging)
         {
-            if ((_healthValue + healPoints) <= MaxHealthValue)
-            {
-                if (_healthValue == 0)
-                    Alived?.Invoke();
-
-                _healthValue += healPoints;
-            }
-            else if (_healthValue + healPoints > MaxHealthValue)
-            {
-                _healthValue = MaxHealthValue;
-            }
-
-            HealthChanged?.Invoke(_healthValue);
+            _isDamaging = false;
+            StopCoroutine(_damageCoroutine);
         }
+
+        if (_isHealing == false)
+            _isHealing = true;
+
+        AssignHealParameters(healPoints);
+        _healCoroutine = StartCoroutine(Healing());
     }
 
     public void TakeDamage(int incomingDamage)
     {
+        if (_isHealing)
+        {
+            _isHealing = false;
+            StopCoroutine(_healCoroutine);
+        }
+
+        if (_isDamaging)
+            StopCoroutine(_damageCoroutine);
+
+        if (_isDamaging == false)
+            _isDamaging = true;
+
+        AssignDamageParameters(incomingDamage);
+        _damageCoroutine = StartCoroutine(Damaging());
+    }
+
+    private void AssignHealParameters(int healPoints)
+    {
+        if (_health < MaxHealthValue)
+        {
+            if (_health + healPoints <= MaxHealthValue)
+                _targetHealth = _health + healPoints;
+            else if (_health + healPoints > MaxHealthValue)
+                _targetHealth = MaxHealthValue;
+        }
+    }
+
+    private void AssignDamageParameters(int incomingDamage)
+    {
         if (incomingDamage > 0)
         {
-            if (_healthValue != 0)
+            if (_health != 0)
             {
-                if (_healthValue >= incomingDamage)
-                {
-                    _healthValue -= incomingDamage;
-                    HealthChanged?.Invoke(_healthValue);
-                }
-                else if (_healthValue < incomingDamage)
-                {
-                    _healthValue = 0;
-                    HealthChanged?.Invoke(_healthValue);
-                }
-
-                if (_healthValue == 0)
-                    Dead?.Invoke();
+                if (_health >= incomingDamage)
+                    _targetHealth = _health - incomingDamage;
+                else if (_health < incomingDamage)
+                    _targetHealth = 0;
             }
+        }
+    }
+
+    private IEnumerator Healing()
+    {
+        while (enabled)
+        {
+            yield return _healingDelay;
+
+            if (_health == 0)
+                Alived?.Invoke();
+
+            _health += _healingValue;
+            HealthChanged?.Invoke(_health);
+
+            if (_health == _targetHealth)
+                break;
+        }
+    }
+
+    private IEnumerator Damaging()
+    {
+        while (enabled)
+        {
+            yield return _damagingDelay;
+
+            _health -= _damagingValue;
+            HealthChanged?.Invoke(_health);
+
+            if (_health == 0)
+                Dead?.Invoke();
+
+            if (_health == _targetHealth)
+                break;
         }
     }
 }
